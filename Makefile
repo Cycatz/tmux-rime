a7a9bee911d837b047e6f94e4c73aa0acf18bc80
Cycatz <thesummernightcat@gmail.com>
2021-06-14 12:09:18 +0800

    Init repo structure

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: build 						#w
+build:
+	python setup.py build_ext --inplace
+
--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # coding=utf-8
 
 from setuptools import Extension, find_packages, setup
-from setuptools.command.build_py import build_py
 
 name = "tmux_rime"
 rime_ext_name = 'rime' 
@@ -15,10 +14,6 @@ rime_ext = Extension(name='{}._{}'.format(name, rime_ext_name), # SWIG requires 
                  extra_compile_args=["-std=c11"],
                  swig_opts=["-outdir", "tmux_rime"])
 
-class BuildPy(build_py):
-    def run(self):
-        self.run_command('build_ext')
-        super(build_py, self).run()
         
 setup (name = name,
        version = '0.1',
@@ -26,8 +21,5 @@ setup (name = name,
        description = """A module to use IME in TMUX with Rime""",
        packages        = [name],
        package_dir     = {'tmux_rime': 'tmux_rime'},
-       cmdclass = {
-            'build_py': BuildPy,
-       },
        ext_modules = [rime_ext],
        )
